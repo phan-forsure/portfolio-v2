e7dbe54b2acd97445f0f0a6e23041bfedbdf1b9c
phan-forsure <153691753+phan-forsure@users.noreply.github.com>
2025-04-30 15:41:40 +0300

    socialButton.tsx の更新

--- app/ui/socialButton.tsx
+++ app/ui/socialButton.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 export default function SocialBtn({
   children,
+  link,
 }: {
   children?: React.ReactNode;
+  link: string;
 }) {
   return (
     <Link
       target={"_blank"}
-      href={"https://github.com/phan-forsure/"}
+      href={link}
       className="p-4 transition-all bg-accent/70 hover:text-accent-foreground hover:bg-accent rounded-lg border-1 border-transparent hover:border-border"
     >
       {children}
